65f891617e1c132626d04021c1db4e3d3fefd550
Max <maximogarciamtnez@gmail.com>
2018-10-08 18:25:06 +0200

    added detection if file doesn't exist

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-import AL
+import sys
+
+import lexico
 
 if __name__ == '__main__':
-    print("HHHHHHH")
+    if(len(sys.argv) != 2):
+        print('You must enter a path to your file like: ./src/script.js\n')
+        sys.exit()
+    else:
+        if(sys.argv[1] == '--help' or sys.argv[1] == '-h'):
+            print('You must enter a path to your file like: ./src/script.js\n')
+            sys.exit()
