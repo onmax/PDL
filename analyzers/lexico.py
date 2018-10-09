5f59daf0b638cb710f401ec77656925d91b32530
max <maximogarciamtnez@gmail.com>
2018-10-09 15:01:55 +0200

    added regex and some tests

--- analyzers/lexico.py
+++ analyzers/lexico.py
@@ -13,22 +13,19 @@ tokens = ["RESERVED_WORD", "STRING", "INTEGER", "OP_PLUS", "OP_DOUBLEPLUS", "OP_
 
 tokens_regex = {
     # Starts with a letter, then it can have 0 or more letters, digit or _
-    'RESERVED_WORD': r"[a-Z][a-Z0-9_]*",
-
+    'RESERVED_WORD': r"^[a-zA-Z][a-zA-Z0-9_]*$",
     #Any character between ""
-    'STRING': r"\".*\"",
-
+    'STRING': r"^\"(.[^(\")]|(\\\")|(?:\s*))*\"$",
     # Must have at least one digit
-    'INTEGER': r"[0-9]+",
-
-    'OP_PLUS': r"+",
-    'OP_DOUBLEPLUS': r"++",
-    'OP_DOUBLEQUAL': r"==",
-    'OP_ANDAND': r"&&",
-    'OP_OPENBRACKETS': r"{",
-    'OP_CLOSEBRACKETS': r"}",
-    'OP_OPENPARENTHESES': r"(",
-    'OP_CLOSEPARENTHESES': r")"
+    'INTEGER': r"^[0-9]+$",
+    "OP_PLUS": r"^\+$",
+    'OP_DOUBLEPLUS': r"^\+\+$",
+    'OP_DOUBLEQUAL': r"^==$",
+    'OP_ANDAND': r"^\&\&$",
+    'OP_OPENBRACKETS': r"^\{$",
+    'OP_CLOSEBRACKETS': r"^\}$",
+    'OP_OPENPARENTHESES': r"^\($",
+    'OP_CLOSEPARENTHESES': r"^\)$"
 }
 
-test_lexico.p()
+test_lexico.test_regex(tokens_regex)
--- test/test_lexico.py
+++ test/test_lexico.py
@@ -1,2 +1,53 @@
-def p():
-    return 0
+import re
+
+def test_regex(tokens):
+    print('Comenzando test de expresiones regulares\n')
+
+    tests = [
+        #1-5
+        test_regex_simple(tokens["RESERVED_WORD"], 'variable', True),                       
+        test_regex_simple(tokens["RESERVED_WORD"], '2variable', False),                     
+        test_regex_simple(tokens["RESERVED_WORD"], '2_variable', False),                    
+        test_regex_simple(tokens["RESERVED_WORD"], '2', False),                             
+        test_regex_simple(tokens["RESERVED_WORD"], '_variable', False),                     
+        #6-10
+        test_regex_simple(tokens["RESERVED_WORD"], 'VAR1', True),                           
+        test_regex_simple(tokens["RESERVED_WORD"], 'VAR_1', True),                          
+        test_regex_simple(tokens["RESERVED_WORD"], '&variable', False),                     
+        test_regex_simple(tokens["RESERVED_WORD"], '', False),                              
+        test_regex_simple(tokens["STRING"], '"Hola, esto es un string"', True),
+        #11-15          
+        test_regex_simple(tokens["STRING"], '"    Esto es un string    "', True),
+        test_regex_simple(tokens["STRING"], 'No es un string"', False),                     
+        test_regex_simple(tokens["STRING"], 'Tampoco es un string', False),                 
+        test_regex_simple(tokens["STRING"], '"Esto deberia ser un error""', False),         
+        test_regex_simple(tokens["STRING"], '"Esto deberia "ser un error"', False),         
+        #16-20                  
+        test_regex_simple(tokens["STRING"], '"Esto deberia " ser un error"', False),         
+        test_regex_simple(tokens["STRING"], '"Y esto " tambien', False),                    
+        test_regex_simple(tokens["STRING"], '"Y esto " tambien"', False),                    
+        test_regex_simple(tokens["STRING"], '"Esto \\" esta bien"', True),                    
+        test_regex_simple(tokens["STRING"], '', False),
+        #21-25                  
+        test_regex_simple(tokens["OP_PLUS"], '+', True),                                    
+        test_regex_simple(tokens["OP_PLUS"], '1+', False),                                  
+        test_regex_simple(tokens["OP_PLUS"], '-', False),                                   
+        test_regex_simple(tokens["OP_PLUS"], '12', False),    
+        #26-30                  
+    ]                             
+   
+
+    for i, test in enumerate(tests):
+        print('Pasado test {0}: {1}'.format(i + 1, test))
+
+# Check if regex is in the test_str and then checks if it should or not.
+def test_regex_simple(regex, test_str, should_be):
+    match = re.findall(regex, test_str, re.MULTILINE)
+    if((len(match) != 0) != should_be):
+        match = [i[0] for i in match]
+        if '' not in match:  
+            print('\tTest: {0}'.format(test_str))
+            print('\tMatch: {0}\n'.format(match))
+        else:
+            return True
+    return (len(match) != 0) == should_be
